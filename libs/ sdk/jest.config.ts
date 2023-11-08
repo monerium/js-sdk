90be96e96e2615ce974706223d23662f62caa6a0
einaralex <einaralex@gmail.com>
2023-11-08 14:50:13 +0000

    fix: detach the sdk repo and add it here

---
+++ libs/ sdk/jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  transformIgnorePatterns: [
+    '/configs/jest.setup.js',
+    '.yarn/',
+    'node_modules/',
+  ],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(t|j|mj)s$',
+  moduleFileExtensions: ['ts', 'js', 'mjs', 'json', 'node'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  // Get a 502 Gateway error when bundle and client tests are run together
+  maxWorkers: 1,
+  coverageDirectory: '../static/coverage',
+  setupFiles: ['./configs/jest.setup.js'],
+  moduleNameMapper: {
+    '^utils$': '<rootDir>/src/utils',
+    '^types$': '<rootDir>/src/types',
+    '^helpers$': '<rootDir>/src/helpers',
+    // Add more aliases as needed
+  },
+};
+export default config;
---
+++ libs/ sdk/src/index.ts
@@ -0,0 +1,4 @@
+export { MoneriumClient } from './client';
+export { default as constants } from './constants';
+export * from './types';
+export { placeOrderMessage, rfc3339 } from './utils';
