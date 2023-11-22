61d93fe9ee8a36c6d571b0b802e64bc148b60f18
einaralex <einaralex@gmail.com>
2023-11-22 14:59:26 +0000

    chore: add create-app init

---
+++ apps/create-app/pages/_app.tsx
@@ -0,0 +1,60 @@
+import '../styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import type { AppProps } from 'next/app';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+  arbitrum,
+  goerli,
+  mainnet,
+  optimism,
+  polygon,
+  base,
+  zora,
+} from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import { MoneriumProvider } from '@monerium/sdk-react-provider';
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    zora,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+  ],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <MoneriumProvider
+          clientId="f99e629b-6dca-11ee-8aa6-5273f65ed05b"
+          environment="sandbox"
+          redirectUrl="http://localhost:4200"
+        >
+          <Component {...pageProps} />
+        </MoneriumProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
